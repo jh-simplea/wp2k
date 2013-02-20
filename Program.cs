d52d2b69c5e25445710f86bfb145319f368006d2
Shauna Gordon <shauna.gordon@fahlgren.com>
2013-02-20 16:55:38 -0500

    Added database entity file. Added start of building database data (code to add entries into BlogPosts and BlogMonth added). All is untested.

--- Program.cs
+++ Program.cs
@@ -10,34 +10,7 @@ using System.Collections.Specialized;
 namespace wp2k {
 	class Program {
 		static void Main(string[] args) {
-			Dictionary<string, string> mapBlogPost = new Dictionary<string,string>() 
-			{
-					{"title", "BlogPostTitle"},
-					{"link", "link"},
-					{"pubDate", "BlogPostDate"},
-					{"dc:creator", "dc:creator"},
-					{"description", "BlogPostSummary"},
-					{"content:encoded", "BlogPostBody"},
-					{"excerpt:encoded", "BlogPostTeaser"},
-					{"wp:post_id", "BlogPostID"},
-					{"wp:post_date", "BlogPostDate"},
-					{"wp:comment_status", "BlogPostAllowComments"}, // NOTE: WP = open/closed; Kentico = 0/1
-					{"wp:status", "status"}, // Published/Not Published; 1/0
-					{"category[domain=\"category\"]", "category"},
-					{"category[domain=\"post_tag\"]", "tag"}
-			};
-
-			Dictionary<string, string> mapBlogComment = new Dictionary<string,string>()
-			{
-				{"wp:comment_id", "CommentID"},
-				{"wp:comment_author", "CommentUserName"}, // CDATA
-				{"wp:comment_author_email", "CommentEmail"},
-				{"wp:comment_author_url", "CommentUrl"},
-				{"wp:comment_date", "CommentDate"},
-				{"wp:comment_content", "CommentText"}, // CDATA
-				{"wp:comment_approved", "CommentApproved"}, // 0/1
-				{"wp:comment_type", "CommentIsTrackBack"} // WP = ""/"pingback"; Kentico = 0/1
-			};
+			// TODO: God functions are evil.
 
 			// Read config file.
 			AppSettingsReader reader = new AppSettingsReader();
@@ -45,59 +18,78 @@ namespace wp2k {
 
 			// Read WPXML file.
 			XDocument wpxml = XDocument.Load(config.Get("file"));
-			var items = from i in wpxml.Descendants("item")
-						select new BlogPost {
-							BlogPostID = Int32.Parse(i.Element("wp:post_id").Value),
-							BlogPostTitle = i.Element("title").Value,
-							BlogPostDate = DateTime.Parse(i.Element("wp:post_date").Value),
-							BlogPostSummary = i.Element("description").Value,
-							BlogPostAllowComments = true,
-							BlogPostBody = i.Element("content:encoded").Value,
-							BlogPostTeaser = i.Element("excerpt:encoded").Value,
-							BlogPostComments = (from c in i.Descendants("wp:comment")
-												select new BlogPostComment { 
-													CommentID = Int32.Parse(c.Element("wp:comment_id").Value),
-													CommentUserName = c.Element("wp:comment_author").Value,
-													CommentEmail = c.Element("wp:comment_author_email").Value,
-													CommentUrl = c.Element("wp:comment_author_url").Value,
-													CommentApproved = Boolean.Parse(c.Element("wp:comment_approved").Value),
-													CommentDate = DateTime.Parse(c.Element("wp:comment_date").Value),
-													CommentText = c.Element("wp:comment_content").Value,
-													CommentIsTrackBack = Boolean.Parse(c.Element("wp:comment_type").Value)
-												})
-						};
-			 
 
-			// Insert data into database.
+			using(kenticofreeEntities context = new kenticofreeEntities()) {
 
+				// Gather list of Posts and make them each Entities
+				var items = (from i in wpxml.Descendants("item")
+						 select new CONTENT_BlogPost {
+							 BlogPostID = Int32.Parse(i.Element("wp:post_id").Value),
+							 BlogPostTitle = i.Element("title").Value,
+							 BlogPostDate = DateTime.Parse(i.Element("wp:post_date").Value),
+							 BlogPostSummary = i.Element("description").Value,
+							 BlogPostAllowComments = true,
+							 BlogPostBody = i.Element("content:encoded").Value,
+							 BlogLogActivity = true
+							 //BlogPostTeaser = i.Element("excerpt:encoded").Value
+						 });
+						//.Union(from c in i.Descendants("wp:comment")
+						//       select new Blog_Comment {
+						//           CommentID = Int32.Parse(c.Element("wp:comment_id").Value),
+						//           CommentUserName = c.Element("wp:comment_author").Value,
+						//           CommentEmail = c.Element("wp:comment_author_email").Value,
+						//           CommentUrl = c.Element("wp:comment_author_url").Value,
+						//           CommentApproved = Boolean.Parse(c.Element("wp:comment_approved").Value),
+						//           CommentDate = DateTime.Parse(c.Element("wp:comment_date").Value),
+						//           CommentText = c.Element("wp:comment_content").Value,
+						//           CommentIsTrackBack = Boolean.Parse(c.Element("wp:comment_type").Value)
+						//       }
+						//);
+
+				// Get the blog by the ID set in the settings
+				CONTENT_Blog blog = (from b in context.CONTENT_Blog
+									 where b.BlogID == Int32.Parse(config.Get("kenticoBlogId"))
+									 select b).First();
+
+				foreach (CONTENT_BlogPost post in items) {
+					// Insert the post into the DB
+					// Why the hell isn't there a bulk update for collections/list of entities?
+					context.CONTENT_BlogPost.AddObject(post);
+					context.SaveChanges();
+
+					// See if there's a BlogMonth entry for the month this post is for
+					var monthQuery = (from m in context.CONTENT_BlogMonth
+											   where m.BlogMonthStartingDate.Year == post.BlogPostDate.Year && m.BlogMonthStartingDate.Month == post.BlogPostDate.Month
+											   select m);
+					CONTENT_BlogMonth month = new CONTENT_BlogMonth();
+					
+					// If not, make a new one
+					if (monthQuery.Any() == false) {
+						month.BlogMonthName = post.BlogPostDate.ToString("MMMM yyyy");
+						month.BlogMonthStartingDate = new DateTime(post.BlogPostDate.Year, post.BlogPostDate.Month, 01);
+
+						context.CONTENT_BlogMonth.AddObject(month);
+						context.SaveChanges();
+					}
+					else {
+						month = monthQuery.First();
+					}
+					
+					CMS_Document doc = new CMS_Document() {
+						DocumentName = post.BlogPostTitle,
+						DocumentNamePath = string.Format("/{0}/{1}/{2}", blog.BlogName, month.BlogMonthName, post.BlogPostTitle),
+						DocumentForeignKeyValue = post.BlogPostID,
+						DocumentCulture = "en-US",
+						DocumentShowInSiteMap = true,
+						DocumentMenuItemHideInNavigation = false,
+						DocumentUseNamePathForUrlPath = false,
+						DocumentStylesheetID = -1
+					};
+				}
+				// Iterate through our Post entities and find each one's corresponding comments
+
+				// Turn the comments in a list of Entities and use the documentID from insert		
+			}
 		}
-
-	}
-
-	public class BlogPost {
-		public int BlogPostID { get; set; }
-		public string BlogPostTitle { get; set; }
-		public DateTime BlogPostDate { get; set; }
-		public string BlogPostSummary { get; set; }
-		public string BlogPostBody { get; set; }
-		public string BlogPostTeaser { get; set; }
-		public bool BlogPostAllowComments { get; set; }
-		public BlogPostComment[] BlogPostComments { get; set; }
-	}
-
-	public class BlogPostComment {
-		public int CommentID { get; set; }
-		public string CommentUserName { get; set; }
-		public int CommentUserID { get; set; }
-		public string CommentUrl { get; set; }
-		public string CommentText { get; set; }
-		public int CommentApprovedByUserID { get; set; }
-		public int CommentPostDocumentID { get; set; }
-		public DateTime CommentDate { get; set; }
-		public bool CommentIsSpam { get; set; }
-		public bool CommentApproved { get; set; }
-		public bool CommentIsTrackBack { get; set; }
-		public string CommentEmail { get; set; }
-		public string CommentInfo { get; set; }
 	}
 }
